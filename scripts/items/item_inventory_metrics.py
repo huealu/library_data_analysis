0614c43f63a65878d5fe129313fce341b928ffe8
huealu <huehong.dinh@gmail.com>
2026-06-24 16:03:52 -0500

    updated items metrics

--- scripts/items/item_inventory_metrics.py
+++ scripts/items/item_inventory_metrics.py
@@ -13,3 +13,21 @@ def volume_count(df: pd.DataFrame, date_col: str) -> list:
     
     count = np.where(df[date_col].notnull(), 1, 0)
     return count
+
+
+def items_have_no_circulations_after_inventory(
+        df: pd.DataFrame, 
+        creation_date_col: str, 
+        circulation_history_col: str) -> list:
+    """
+    Check if items have no circulation after the inventory date.
+    """
+    if creation_date_col not in df.columns:
+        raise ValueError(f"Column '{creation_date_col}' does not exist in the DataFrame.")
+    if circulation_history_col not in df.columns:
+        raise ValueError(f"Column '{circulation_history_col}' does not exist in the DataFrame.")
+    
+    no_circulation = np.where((df[creation_date_col] > datetime.now()) 
+                              & (df[circulation_history_col] == 0), 1, 0)
+    
+    return no_circulation
